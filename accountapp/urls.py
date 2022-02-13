27d2b3f567422dd97e572d8b274677bb0d9c99f2
robbert9301 <chg9301@gmail.com>
2022-02-13 19:16:55 +0900

    django course 21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create') #class는 accountcrateview를 써야한다
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 
@@ -23,3 +26,9 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') #account creation에 성공했다면 그이후에 어디로 연결할 것인지에 대한 구문 & class에서는 reverse를 쓰지 못한다
+    template_name = 'accountapp/create.html'
