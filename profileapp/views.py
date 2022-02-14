e1cf6c23a6004f8547ea2d3bc8772c15362170d6
robbert9301 <chg9301@gmail.com>
2022-02-14 18:05:43 +0900

    django course 32 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -15,8 +17,18 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
-    def form_valid(self, form): #profile form에서 user를 사용하지 않았는데 이미지의 주인이 누구인지 알게하기 위해서 서버내에서 구현하기 위함
-        temp_profile = form.save(commit=False) #임시로 form 을 저장
+    def form_valid(self, form):  # profile form에서 user를 사용하지 않았는데 이미지의 주인이 누구인지 알게하기 위해서 서버내에서 구현하기 위함
+        temp_profile = form.save(commit=False)  # 임시로 form 을 저장
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
