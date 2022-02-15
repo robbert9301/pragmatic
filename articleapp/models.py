38128077cbb678c0d8be1c35d554e1290476624c
robbert9301 <chg9301@gmail.com>
2022-02-15 20:12:37 +0900

    django course 35 commit

---
+++ articleapp/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) #on_delete는 사용자가 없어지더라도 그대로 게시물은 남아있게하기위함
+    # project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
